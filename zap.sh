f188f2613cd5c46254a0979975cecd8dcd785c9f
sandeepkumar1234 <54533323+sandeepkumar1234@users.noreply.github.com>
2021-08-06 16:39:12 +0530

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-stable zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
+CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
 
 # the target URL for ZAP to scan
 # the target URL for ZAP to scan
@@ -27,7 +27,7 @@ docker cp $CONTAINER_ID:zap/result.xml ./
 echo "ELK stack"
 until curl esurl  ; do echo "Waiting for Elastic Search"; sleep 2; done
 
-cp result.json zap/ && cd  zap
+cp result.json zap1/ && cd  zap1
 echo "parse result.json - add indices"
 cat result.json | jq -c '.[] | {"index": {"_index": "zapindex", "_type": "zapindex", "_id": "_id"}}, .' | curl -H 'Content-Type: application/json'   -XPOST esurl/_bulk --data-binary @-
 
